31c595f8f8b2380a14ccd204dd584b5f57c1fc31
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-06-14 02:38:05 -0400

    Una pratica con un toggle

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+	const [Show, setShow] = useState(false);
+	return (
+		<>
+			<button className='btn' onClick={() => setShow(!Show)}>
+				Show/Hide
+			</button>
+			{Show && <Item />}
+		</>
+	);
+};
+
+const Item = () => {
+	const [Size, setSize] = useState(window.innerWidth);
+
+  const CheckSize = () => {
+    setSize(window.innerWidth);
+  }
+
+	useEffect(() => {
+		window.addEventListener('resize', CheckSize);
+		return () => {
+			window.removeEventListener('resize', CheckSize);
+		};
+	}, []);
+  
+	return (
+		<div style={{ marginTop: '2rem' }}>
+			<h1>window</h1>
+			<h2>size: {Size}PX</h2>
+		</div>
+	);
 };
 
 export default ShowHide;
