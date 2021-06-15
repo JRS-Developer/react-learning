f6075b1fa15f6babed8532ff2edaf913d4616770
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-06-15 15:13:17 -0400

    Usando prop drilling, el infierno mismo de pasar props. Ahora vamos con Context API :D

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,48 @@
 import React, { useState } from 'react';
+import { data } from '../../../data';
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+	const [people, setPeople] = useState(data);
+
+	const removePerson = (id) => {
+		setPeople((pastPeople) => {
+			return pastPeople.filter((person) => person.id !== id);
+		});
+	};
+
+	return (
+		<section>
+			<h3>prop drilling</h3>
+			<List people={people} removePerson={removePerson} />
+		</section>
+	);
 };
 
+const List = ({ people, removePerson }) => {
+	return (
+		<ul>
+			{people.map((person) => {
+				return (
+					<SinglePerson
+						{...person}
+						key={person.id}
+						removePerson={removePerson}
+					/>
+				);
+			})}
+		</ul>
+	);
+};
+
+const SinglePerson = ({ id, name, removePerson }) => {
+	return (
+		<li className='item'>
+			<h4>{name}</h4>
+			<button onClick={() => removePerson(id)}>Remove Item</button>
+		</li>
+	);
+};
 export default PropDrilling;
