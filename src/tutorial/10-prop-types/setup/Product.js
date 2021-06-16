378110ac6f55269a7f693649ca329bd605550e27
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-06-16 16:26:49 -0400

    Terminado PropTypes

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ name, price, image }) => {
+	const url = image && image.url;
+
+	return (
+		<article className='product'>
+			<img src={url || defaultImage} alt={name || 'default'} />
+			<h4>{name || 'default'}</h4>
+			<p>{price || 3.99}</p>
+		</article>
+	);
 };
+
+// Product.propTypes = {
+// 	image: PropTypes.object.isRequired,
+// 	name: PropTypes.string.isRequired,
+// 	price: PropTypes.number.isRequired,
+// };
+
+// Product.defaultProps = {
+// 	image: {
+// 		url: defaultImage,
+// 	},
+// 	name: 'Default Name',
+// 	price: 3.99,
+// };
 
 export default Product;
--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -6,13 +6,13 @@ import { useFetch } from "./2-useFetch";
 const url = "https://course-api.com/javascript-store-products";
 
 const Example = () => {
-  const { products, loading } = useFetch(url);
-  console.log(products);
-  return (
-    <div>
-      <h2>{loading ? "loading..." : "data"}</h2>
-    </div>
-  );
+    const { products, loading } = useFetch(url);
+    console.log(products);
+    return (
+        <div>
+            <h2>{loading ? "loading..." : "data"}</h2>
+        </div>
+    );
 };
 
 export default Example;
